9f396c2ce869bd6c9440441829a4b297108cd895
Enrique <enrique.davila@gmail.com>
2019-12-20 16:06:34 -0500

    new file added to chec if date is greater that other

---
+++ lib/dateDiffMonths.go
@@ -0,0 +1,26 @@
+package lib
+import t "time"
+import f "fmt"
+func main(){
+	var initialDate, endDate string
+	layout := "2006-01-02"
+	f.Println("Capture start Date: ")
+	f.Scanln(&initialDate)
+	f.Println("Capture end Date")
+	f.Scanln(&endDate)
+	f.Println("Your inputs: Start Date" , initialDate,  " End Date: " ,  endDate)
+	t1, _ := t.Parse(layout, initialDate)
+	t2, _ := t.Parse(layout, endDate)
+	//var x int
+	tempdate:=t1
+	if tempdate.Before(t2){
+	 f.Println("Date 1 is lower than date 2")
+	}else{
+	f.Println("Date 1 is greater than date 2")
+	}
+	for x:=0;tempdate.Before(t2);x++{
+	tempdate = t1.AddDate(0,0,x)
+	f.Println(tempdate)
+	
+	}
+	
--- lib/incident_resolution.go
+++ lib/incident_resolution.go
@@ -34,8 +34,19 @@ func DTimeDiff(startDate string, endDate string) float64{ //get time difference 
 		//f.Println("Date 2: ",t2)
 		hours := t2.Sub(t1).Hours() 
 		return hours
+}
 
+func MTimeDiff(startDate string, endDate string) float64{ //get time difference in Months
 
-
-
+	input1 := startDate	
+	input2 := endDate
+	layout := "2006-01-02"
+	t1, _ := t.Parse(layout, input1)
+	t2, _ := t.Parse(layout, input2)
+	//f.Println("Parameter1: ",startDate)
+	//f.Println("Parameter2: ",endDate)
+	//f.Println("Date 1: ",t1)
+	//f.Println("Date 2: ",t2)
+	months := t2.Sub(t1).Hours() /24
+	return months
 }
