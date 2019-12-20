aca0bdf41a90512ec67afe750556bbbcfce48979
Enrique <enrique.davila@gmail.com>
2019-12-20 14:33:43 -0500

    First Commit

---
+++ lib/incident_resolution.go
@@ -0,0 +1,41 @@
+package lib
+
+
+import t "time"
+//import f "fmt"
+
+
+
+func DTimeDiff(startDate string, endDate string) float64{ //get time difference in days
+
+	input1 := startDate	
+	input2 := endDate
+	layout := "2006-01-02"
+	t1, _ := t.Parse(layout, input1)
+	t2, _ := t.Parse(layout, input2)
+	//f.Println("Parameter1: ",startDate)
+	//f.Println("Parameter2: ",endDate)
+	//f.Println("Date 1: ",t1)
+	//f.Println("Date 2: ",t2)
+	days := t2.Sub(t1).Hours() / 24
+	return days
+}
+
+	func HTimeDiff(startDate string, endDate string) float64{ //get time difference in days
+
+		input1 := startDate	
+		input2 := endDate
+		layout := "2006-01-02"
+		t1, _ := t.Parse(layout, input1)
+		t2, _ := t.Parse(layout, input2)
+		//f.Println("Parameter1: ",startDate)
+		//f.Println("Parameter2: ",endDate)
+		//f.Println("Date 1: ",t1)
+		//f.Println("Date 2: ",t2)
+		hours := t2.Sub(t1).Hours() 
+		return hours
+
+
+
+
+}
---
+++ main.go
@@ -0,0 +1,25 @@
+package main
+
+import f "fmt"
+import "github.com/oblicore/lib"
+
+func main(){
+
+	var initialDate string
+	var endDate string
+	
+
+	//lib.CaptureName("01/01/2019","02/01/2019")
+	f.Println("Capture start Date: ")
+	f.Scanln(&initialDate)
+	f.Println("Capture end Date")
+	f.Scanln(&endDate)
+	f.Println("Your inputs: Start Date" , initialDate,  " End Date: " ,  endDate)
+	days:=lib.DTimeDiff(initialDate,endDate)
+	hours:=lib.HTimeDiff(initialDate,endDate)
+	f.Println("Difference in days: " , days) 
+	f.Println("Difference in Hours: " , hours) 
+
+
+
+}
