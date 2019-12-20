93e3ca8c581a93699f0e72fa06abfb51baf4af00
Enrique <enrique.davila@gmail.com>
2019-12-20 16:40:27 -0500

    Months between working

--- main.go
+++ main.go
@@ -17,8 +17,11 @@ func main(){
 	f.Println("Your inputs: Start Date" , initialDate,  " End Date: " ,  endDate)
 	days:=lib.DTimeDiff(initialDate,endDate)
 	hours:=lib.HTimeDiff(initialDate,endDate)
+	months:=lib.MTimeDiff(initialDate,endDate)
 	f.Println("Difference in days: " , days) 
 	f.Println("Difference in Hours: " , hours) 
+	f.Println("Difference in Months: " , months) 
+
 
 
 
